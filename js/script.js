62c268dd035d52f848d44371ef2f60d474479c59
sergiownd <sergiownd94@gmail.com>
2016-04-14 12:56:35 +0200

    mejoras graves
    usuario: usuario
    contraseña: 1234
    
    Que por añadir el botón de salir a los juegos (ver el juego de
    alimentación, copiar el <a del html y eliminar el final del .lim)
    Añadir el texto: Pulse el botón de X para salir.
    Encontrar algún sonido que diga: Hola amigo al principio y algo en el
    menú al clickar en los muñecos.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
 var volume = true;
 
 $(document).ready(function(){
-    $(".muñeco").click(function(){
-        $(".panel").css("display", "block");
-        $(".bienvenido").css("display", "block");
-    });
-    $("#ayuda").click(function(){
-        open('ayuda.html','','top=300,left=300,width=300,height=300') ; 
-
-    });
-    $("#salir").click(function(){
-        open('salir.html','','top=300,left=300,width=300,height=300') ; 
-
-    });
     $("#volumen-img").click(function () {
         if (volume == true){
             $(this).attr("src","../image/volume-off.png");
@@ -22,22 +10,76 @@ $(document).ready(function(){
             $(this).attr("src","../image/volumen.png");
             volume = true;
         }
+        console.log(volume);
     });
-    var audioElement = document.createElement('audio');
-    audioElement.setAttribute('src', '../');
-    audioElement.setAttribute('autoplay', 'autoplay');
-    //audioElement.load()
-    $.get();
+    $(".muñeco").click(function(){
+        $(".panel").css("display", "block");
+        $(".bienvenido").css("display", "block");
+        
+        if (volume == true){
+            var audioElement = document.createElement('audio');
+            audioElement.setAttribute('src', '../juegos/Cartoon_Winning_Sound_Effect.mp3');
+            audioElement.setAttribute('autoplay', 'autoplay');
+            //audioElement.load()
 
-    audioElement.addEventListener("load", function() {
-        audioElement.Play();
-    }, true);
+            $.get();
 
-    $('.play').click(function() {
-        audioElement.Play();
+            audioElement.addEventListener("load", function() {
+                audioElement.Play();
+            }, true);
+
+            $('.play').click(function() {
+                audioElement.Play();
+            });
+
+            $('.pause').click(function() {
+                audioElement.Stop();
+            });
+        }
+    });
+    $(".muñeco2").click(function(){
+        $(".panel").css("display", "block");
+        $(".bienvenido").css("display", "block");
+        
+        if (volume == true){
+            var audioElement = document.createElement('audio');
+            audioElement.setAttribute('src', '../juegos/Cartoon_Winning_Sound_Effect.mp3');
+            audioElement.setAttribute('autoplay', 'autoplay');
+            //audioElement.load()
+
+            $.get();
+
+            audioElement.addEventListener("load", function() {
+                audioElement.Play();
+            }, true);
+
+            $('.play').click(function() {
+                audioElement.Play();
+            });
+
+            $('.pause').click(function() {
+                audioElement.Stop();
+            });
+        }
+    });
+    $("#ayuda").click(function(){
+        open('ayuda.html','','top=300,left=300,width=300,height=300') ; 
+
+    });
+    $("#salir").click(function(){
+        open('salir.html','','top=300,left=300,width=300,height=300') ; 
+
     });
     
-    $('.pause').click(function() {
-        audioElement.Stop();
+    $("#entra").click(function(){
+        if($("#imput-n").val() === "usuario"){
+            if ($("#imput-a").val() === "1234"){
+                $("#entra").attr('href', './menu.html');
+            }
+        }
+        else{
+            $(".error").css("display","block");
+        }
     });
+    
 });
